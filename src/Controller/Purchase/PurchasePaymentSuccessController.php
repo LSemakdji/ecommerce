fec85aa5e09ed352169ee93f0352c4b9ef7c9a97
Lounes <lounes.semakdji@laposte.net>
2020-12-07 10:58:51 +0100

    Mise en place des évenements

--- src/Controller/Purchase/PurchasePaymentSuccessController.php
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -4,11 +4,13 @@ namespace App\Controller\Purchase;
 
 use App\Entity\Purchase;
 use App\Cart\CartService;
+use App\Event\PurchaseSuccessEvent;
 use App\Repository\PurchaseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class PurchasePaymentSuccessController extends AbstractController{
 
@@ -16,18 +18,19 @@ class PurchasePaymentSuccessController extends AbstractController{
      * @Route("/purchase/terminate/{id}", name="purchase_payment_success")
      * @IsGranted("ROLE_USER")
      */
-    public function success($id, PurchaseRepository $purchaseRepository,EntityManagerInterface $em, CartService $cartService){
+    public function success($id, PurchaseRepository $purchaseRepository,EntityManagerInterface $em, CartService $cartService, EventDispatcherInterface $dispatcher)
+    {
         
         // 1. Je récupère la commande 
-        $purchase = $purchaseRepository->find($id);
-        if (!$purchase || 
-            ($purchase && $purchase->getUser() !== $this->getUser())||
-            ($purchase && $purchase->getStatus() === Purchase::STATUS_PAID)
-            ){
-            $this->addFlash('warning', "La commande n'existe pas");
+            $purchase = $purchaseRepository->find($id);
+            if (!$purchase || 
+                ($purchase && $purchase->getUser() !== $this->getUser())||
+                ($purchase && $purchase->getStatus() === Purchase::STATUS_PAID)
+                ){
+                    $this->addFlash('warning', "La commande n'existe pas");
 
-            return $this->redirectToRoute("purchases_index");
-            }
+                    return $this->redirectToRoute("purchases_index");
+                }
 
         // 2. Je l'a fait passer au status payé(PAID) et je l'envoie en BDD
             $purchase->setStatus(Purchase::STATUS_PAID);
@@ -38,11 +41,15 @@ class PurchasePaymentSuccessController extends AbstractController{
         // 3. Je vide le panier
             $cartService->empty();
 
+        // Lancer un évenement qui permette au developpeur de réagir à la prise d'une commande   
+            $purchaseEvent= new PurchaseSuccessEvent($purchase);
+            $dispatcher->dispatch($purchaseEvent,'purchase.success');
+        
         // 4. Je redirige avec un flash vers la liste des commandes
             $this->addFlash('success',"La commande a été payée et confirmée !");
             return $this->redirectToRoute("purchases_index");
 
-        }
+    }
 
 
 }
